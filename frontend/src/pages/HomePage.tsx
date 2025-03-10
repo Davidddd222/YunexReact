347700c44f18f95ca37ddd49b25553fdc481b927
Davidddd222 <davidvergaranacun9@gmail.com>
2025-03-10 08:33:12 -0500

    Lunes 10

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage: React.FC = () => {
     return (
         <>
         <Header />
-        <div className='text-center justify-center italic text-2xl font-semibold'>
+        <div className=' bg-white text-center justify-center italic text-2xl font-semibold'>
             <h1>Welcome to the Home Page</h1>
             <p>This is the home page of our application.</p>
         </div>
